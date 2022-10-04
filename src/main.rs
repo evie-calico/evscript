5071b006c0b73b38a9415cb0f7c0014e3d3fbb9c
Eievui <14899090+eievui5@users.noreply.github.com>
2022-10-04 19:33:27 -0400

    Complete Parser

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,125 @@
-extern crate lalrpop_util;
-
-use lalrpop_util::lalrpop_mod;
-
-lalrpop_mod!(pub parser);
-
 use std::process::exit;
 
 fn main() {
-	let input = "env name { def hi(); def bye(); }";
-	let result = parser::EnvParser::new().parse(input);
-	match result {
+	let input = r#"
+include "../include/script.evs";
+#asm
+	include "defines.inc"
+	include "dungeon.inc"
+#end
+
+script fn xGenerateScraper() {
+	u8 x = 32;
+	u8 y = 32;
+
+	repeat 255 {
+		step_direction(rand() & 3, x, y);
+		map_put_tile(x, y, TILE_CLEAR);
+	}
+
+	map_put_tile(x, y, TILE_EXIT);
+}
+
+script fn xGenerateHalls() {
+	u8 direction = rand() & 3;
+
+	u8 width = rand() & 3 + 4;
+	u8 height = rand() & 3 + 4;
+
+	u8 x = 32 - (rand() & 3);
+	u8 y = 32 - (rand() & 3);
+	
+	repeat height {
+		repeat width {
+			map_put_tile(x, y, TILE_CLEAR);
+			x += 1;
+		}
+		x -= width;
+		y += 1;
+	}
+
+	repeat 9 {
+		repeat rand() & 1 + 2 {
+			repeat rand() & 7 + 4 {
+				map_put_tile(x, y, TILE_CLEAR);
+				step_direction(direction, x, y);
+			}
+
+			direction += (rand() & 2) - 1 & 3;
+		}
+
+		u8 old_x = x;
+		u8 old_y = y;
+
+		width = (rand() & 3) + 3;
+		height = (rand() & 3) + 3;
+		
+		repeat height {
+			repeat width {
+				map_put_tile(x, y, TILE_CLEAR);
+				x += 1;
+			}
+			x -= width;
+			y += 1;
+		}
+
+		x = old_x;
+		y = old_y;
+
+		if direction == UP {
+			y -= 1;
+		} else if direction == LEFT {
+			x += width;
+		} else if direction == DOWN {
+			x += width - 1;
+			y += height;
+		} else if direction == RIGHT {
+			x -= 1;
+			y += height - 1;
+		}
+	}
+
+	width = 3;
+	height = 3;
+	x -= 1;
+	y -= 1;
+
+	repeat height {
+		repeat width {
+			map_put_tile(x, y, TILE_CLEAR);
+			x += 1;
+		}
+		x -= width;
+		y += 1;
+	}
+	x += 1;
+	y -= 2;
+	map_put_tile(x, y, TILE_EXIT);
+}
+
+script fn xGenerateItems() {
+	do {
+		u8 x = rand() & 63;
+		u8 y = rand() & 63;
+		u8 tile = map_get_tile(x, y);
+	} while tile != TILE_CLEAR;
+
+	// The items are given a weight in the following order:
+	// ITEM0 - 6/16, 37.50% chance
+	// ITEM1 - 6/16, 37.50% chance
+	// ITEM2 - 3/16, 18.75% chance
+	// ITEM3 - 1/16,  6.25% chance
+
+	// Clamp the random number to (0, 15) and choose an item.
+	u8 item_id = rand() & 15;
+
+	if item_id < 6 { map_put_tile(x, y, TILE_ITEM0); }
+	else if item_id < 12 { map_put_tile(x, y, TILE_ITEM1); }
+	else if item_id < 15 { map_put_tile(x, y, TILE_ITEM2); }
+	else { map_put_tile(x, y, TILE_ITEM3); }
+}
+"#;
+	match evscript::parse(input) {
 		Ok(ast) => println!("{ast:#?}"),
 		Err(err) => {
 			eprintln!("<test>: {err}");
@@ -20,43 +130,174 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-	use evscript::lexer::Location;
-	use evscript::parser::parse;
-
 	#[test]
-	fn syntax() {
-		// Test all basic lexing & parsing.
-		let mut loc = Location::new("<syntax test>");
-		let mut input = "
-			#asm
-				include \"hardware.inc\"
-			#end
+	fn example_header() {
+		let input = r#"
+#asm include "script.inc" #end
 
-			include asm \"hardware.inc\"
+env script {
+	use std;
+	def memset(ptr, u8, u16);
+	def rand(u8);
+	def print(ptr);
+	def say(ptr);
+	def print_wait();
+	def get_flag(u8, u8);
+	def map_put_tile(u8, u8, u8);
+	def map_get_tile(u8, u8, u8);
+	def step_direction(u8, u8, u8);
+	def draw_sprite(u8);
+	def npc_move(u8, u16, u16);
+	def npc_set_frame(u8, u8);
+	def npc_lock();
 
-			env script {
-				use std;
-				def example(u8);
-			}
+	pool = 16;
+	terminator = return;
+}
 
-			script fn Main {}
-			".chars().peekable();
+env npc {
+	use script;
+	macro move(...) = npc_move;
+	macro set_frame(...) = npc_set_frame;
+	alias lock() = npc_lock();
+	alias wait() = print_wait();
+	pool = 8;
+}
+"#;
 
-		if let Err(err) = parse(&mut input, &mut loc){
-			eprintln!("{loc}: {err}");
-			panic!("{loc}: {err}");
+		if let Err(err) = evscript::parse(input) {
+			panic!("<example_header>: {err}");
 		}
 	}
 
 	#[test]
-	fn syntax() {
-		// Test all basic lexing & parsing.
-		let mut loc = Location::new("<expression test>");
-		let mut input = "script fn Main { x + y; }".chars().peekable();
+	fn example_script() {
+		let input = r#"
+include "../include/script.evs";
+#asm
+	include "res/charmap.inc"
+	include "entity.inc"
+#end
 
-		if let Err(err) = parse(&mut input, &mut loc){
-			eprintln!("{loc}: {err}");
-			panic!("{loc}: {err}");
+npc fn xWalkAround() {
+	lock();
+	say("Hello, World!<WAITBTN>");
+	wait();
+	set_frame("self", ENTITY_FRAME_STEP);
+	repeat 60 {
+		move("self", 12, 0);
+		yield;
+	}
+	set_frame("self", ENTITY_FRAME_IDLE);
+	repeat 60 {
+		yield;
+	}
+	set_frame("self", ENTITY_FRAME_STEP);
+	repeat 60 {
+		move("self", -12, 0);
+		yield;
+	}
+	set_frame("self", ENTITY_FRAME_IDLE);
+	say("Bye!<WAITBTN><CLEAR>");
+	wait();
+	lock();
+}
+"#;
+
+		if let Err(err) = evscript::parse(input) {
+			panic!("<example_script>: {err}");
+		}
+	}
+
+	#[test]
+	fn example_function() {
+		let input = r#"
+include "../include/script.evs";
+#asm
+	include "defines.inc"
+	include "dungeon.inc"
+#end
+
+script fn xGenerateHalls() {
+	u8 direction = rand() & 3;
+
+	u8 width = rand() & 3 + 4;
+	u8 height = rand() & 3 + 4;
+
+	u8 x = 32 - (rand() & 3);
+	u8 y = 32 - (rand() & 3);
+	
+	repeat height {
+		repeat width {
+			map_put_tile(x, y, TILE_CLEAR);
+			x += 1;
+		}
+		x -= width;
+		y += 1;
+	}
+
+	repeat 9 {
+		repeat rand() & 1 + 2 {
+			repeat rand() & 7 + 4 {
+				map_put_tile(x, y, TILE_CLEAR);
+				step_direction(direction, x, y);
+			}
+
+			direction += (rand() & 2) - 1 & 3;
+		}
+
+		u8 old_x = x;
+		u8 old_y = y;
+
+		width = (rand() & 3) + 3;
+		height = (rand() & 3) + 3;
+		
+		repeat height {
+			repeat width {
+				map_put_tile(x, y, TILE_CLEAR);
+				x += 1;
+			}
+			x -= width;
+			y += 1;
+		}
+
+		x = old_x;
+		y = old_y;
+
+		if direction == UP {
+			y -= 1;
+		} else if direction == LEFT {
+			x += width;
+		} else if direction == DOWN {
+			x += width - 1;
+			y += height;
+		} else if direction == RIGHT {
+			x -= 1;
+			y += height - 1;
+		}
+	}
+
+	width = 3;
+	height = 3;
+	x -= 1;
+	y -= 1;
+
+	repeat height {
+		repeat width {
+			map_put_tile(x, y, TILE_CLEAR);
+			x += 1;
+		}
+		x -= width;
+		y += 1;
+	}
+	x += 1;
+	y -= 2;
+	map_put_tile(x, y, TILE_EXIT);
+}
+"#;
+
+		if let Err(err) = evscript::parse(input) {
+			panic!("<example_script>: {err}");
 		}
 	}
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,25 +1,34 @@
 #![allow(dead_code)]
 
-use crate::lexer::{lex, Location, Token};
-
-use std::iter::Peekable;
 use std::vec::Vec;
-use std::str::Chars;
 
 #[derive(Debug)]
 pub enum Statement {
-	Block(Vec<Statement>),
-	// Top-level statements.
-	Environment(Environment),
-	Function(Function),
-	InlineAssembly(String),
 	// Environment statements
 	Use(String),
 	Def(Def),
+	Alias(Alias),
+	Macro(Macro),
+	Pool(Rpn),
+	Terminator(String),
 	// Function statements
-	VariableDeclaration(VariableDeclaration),
-	// Generic statements
-	Expression(Rpn)
+	Expression(Rpn),
+	Declaration(String, String),
+	DeclareAssign(String, Box<Statement>),
+	If(Rpn, Vec<Statement>, Option<Vec<Statement>>),
+	While(Rpn, Vec<Statement>),
+	Do(Rpn, Vec<Statement>),
+	For(Box<Statement>, Rpn, Box<Statement>, Vec<Statement>),
+	Repeat(Option<String>, Rpn, Vec<Statement>),
+	Loop(Vec<Statement>),
+}
+
+#[derive(Debug)]
+pub enum Root {
+	Environment(Environment),
+	Function(Function),
+	Assembly(String),
+	Include(String),
 }
 
 // Top-level statements.
@@ -31,9 +40,10 @@ pub struct Environment {
 
 #[derive(Debug)]
 pub struct Function {
-	name: String,
-	environment: String,
-	contents: Vec<Statement>
+	pub name: String,
+	pub environment: String,
+	pub args: Vec<String>,
+	pub contents: Vec<Statement>
 }
 
 // Environment statements
@@ -43,11 +53,20 @@ pub struct Def {
 	pub args: Vec<String>,
 }
 
-// Function statements
 #[derive(Debug)]
-pub struct VariableDeclaration {
-	name: String,
-	var_type: String,
+pub struct Macro {
+	pub name: String,
+	pub args: Vec<String>,
+	pub target: String,
+	pub varargs: bool,
+}
+
+#[derive(Debug)]
+pub struct Alias {
+	pub name: String,
+	pub args: Vec<String>,
+	pub target: String,
+	pub target_args: Vec<String>,
 }
 
 #[derive(Debug)]
@@ -56,225 +75,37 @@ pub enum Rpn {
 	Variable(String),
 	Unsigned(u64),
 	Signed(i64),
-	// Unary Operations
+	String(String),
+	Call(String, Vec<Rpn>),
+	// Unary
 	Negate(Box<Rpn>),
 	Deref(Box<Rpn>),
+	Not(Box<Rpn>),
 	Address(String),
-	// Binary Operations
-	Add(Box<Rpn>, Box<Rpn>),
-	Sub(Box<Rpn>, Box<Rpn>),
+	// Factors
 	Mul(Box<Rpn>, Box<Rpn>),
 	Div(Box<Rpn>, Box<Rpn>),
 	Mod(Box<Rpn>, Box<Rpn>),
+	// Addition
+	Add(Box<Rpn>, Box<Rpn>),
+	Sub(Box<Rpn>, Box<Rpn>),
+	// Shifts
+	ShiftLeft(Box<Rpn>, Box<Rpn>),
+	ShiftRight(Box<Rpn>, Box<Rpn>),
+	// Binaries
+	BinaryAnd(Box<Rpn>, Box<Rpn>),
+	BinaryXor(Box<Rpn>, Box<Rpn>),
+	BinaryOr(Box<Rpn>, Box<Rpn>),
+	// Compares
 	Equ(Box<Rpn>, Box<Rpn>),
 	NotEqu(Box<Rpn>, Box<Rpn>),
+	LessThan(Box<Rpn>, Box<Rpn>),
+	GreaterThan(Box<Rpn>, Box<Rpn>),
+	LessThanEqu(Box<Rpn>, Box<Rpn>),
+	GreaterThanEqu(Box<Rpn>, Box<Rpn>),
+	// Logicals
 	LogicalAnd(Box<Rpn>, Box<Rpn>),
 	LogicalOr(Box<Rpn>, Box<Rpn>),
 	// += is constructed using a Set(self, Add(self, <expression>))
 	Set(String, Box<Rpn>),
-}
-
-impl Rpn {
-	fn precedence(&self) -> u32 {
-		match self {
-			Rpn::Variable(..) => 0,
-			Rpn::Unsigned(..) => 0,
-			Rpn::Signed(..) => 0,
-			Rpn::Negate(..) => 1,
-			Rpn::Deref(..) => 1,
-			Rpn::Address(..) => 1,
-			Rpn::Mul(..) => 2,
-			Rpn::Div(..) => 2,
-			Rpn::Mod(..) => 2,
-			Rpn::Add(..) => 3,
-			Rpn::Sub(..) => 3,
-			Rpn::Equ(..) => 4,
-			Rpn::NotEqu(..) => 4,
-			Rpn::LogicalAnd(..) => 5,
-			Rpn::LogicalOr(..) => 6,
-			Rpn::Set(..) => 7,
-		}
-	}
-}
-
-fn expect_identifier(input: &mut Peekable<Chars>, loc: &mut Location) -> Result<String, String> {
-	let token = lex(input, loc)?;
-
-	match token {
-		Token::Identifier(name) => Ok(name),
-		_ => Err(format!("Expected Identifier, got {token}"))
-	}
-}
-
-fn expect_string(input: &mut Peekable<Chars>, loc: &mut Location) -> Result<String, String> {
-	let token = lex(input, loc)?;
-
-	match token {
-		Token::String(string) => Ok(string),
-		_ => Err(format!("Expected String, got {token}"))
-	}
-}
-
-fn expect_lparen(input: &mut Peekable<Chars>, loc: &mut Location) -> Result<(), String> {
-	let token = lex(input, loc)?;
-
-	if token != Token::LeftParenthesis {
-		Err(format!("Expected (, got {token}"))
-	} else {
-		Ok(())
-	}
-}
-
-fn expect_lbrace(input: &mut Peekable<Chars>, loc: &mut Location) -> Result<(), String> {
-	let token = lex(input, loc)?;
-
-	if token != Token::LeftCurlyBrace {
-		Err(format!("Expected {{, got {token}"))
-	} else {
-		Ok(())
-	}
-}
-
-fn expect_semicolon(input: &mut Peekable<Chars>, loc: &mut Location) -> Result<(), String> {
-	let token = lex(input, loc)?;
-
-	if token != Token::Semicolon {
-		Err(format!("Expected ;, got {token}"))
-	} else {
-		Ok(())
-	}
-}
-
-// For binary operations; checks for an operator and constructs an Rpn node for it.
-fn parse_operation(input: &mut Peekable<Chars>, loc: &mut Location, lhs: Rpn) -> Result<Rpn, String> {
-	match lex(input, loc)? {
-		Token::Plus => Ok(Rpn::Add(Box::new(lhs), Box::new(parse_expression(lex(input, loc)?, input, loc)?))),
-		Token::Star => Ok(Rpn::Mul(Box::new(lhs), Box::new(parse_expression(lex(input, loc)?, input, loc)?))),
-		Token::Semicolon => Ok(lhs),
-		token @ _ => return Err(format!("Unexpected {token}"))
-	}
-}
-
-fn parse_expression(token: Token, input: &mut Peekable<Chars>, loc: &mut Location) -> Result<Rpn, String> {
-	// Unary context
-	let lhs = match token {
-		Token::Identifier(identifier) => Rpn::Variable(identifier),
-		Token::Minus => Rpn::Negate(Box::new(parse_expression(lex(input, loc)?, input, loc)?)),
-		Token::Star => Rpn::Deref(Box::new(parse_expression(lex(input, loc)?, input, loc)?)),
-		_ => return Err(format!("Unexpected {token}"))
-	};
-
-	parse_operation(input, loc, lhs)
-}
-
-fn parse_environment(input: &mut Peekable<Chars>, loc: &mut Location) -> Result<Vec<Statement>, String> {
-	let mut ast = Vec::<Statement>::new();
-
-	loop {
-		match lex(input, loc)? {
-			Token::Use => {
-				ast.push(Statement::Use(expect_identifier(input, loc)?));
-				expect_semicolon(input, loc)?;
-			}
-
-			Token::Def => {
-				let name = expect_identifier(input, loc)?;
-				expect_lparen(input, loc)?;
-
-				let mut args = Vec::<String>::new();
-
-				loop {
-					match lex(input, loc)? {
-						Token::RightParenthesis => break,
-						Token::Identifier(string) => args.push(string),
-						token @ _ => return Err(format!("Unexpected {token} in parameter list")),
-					}
-				}
-
-				expect_semicolon(input, loc)?;
-
-				ast.push(Statement::Def(Def { name, args }));
-			}
-
-			Token::RightCurlyBrace => break,
-			token @ _ => return Err(format!("Unexpected {token}"))
-		}
-	}
-
-	Ok(ast)
-}
-
-fn parse_function(input: &mut Peekable<Chars>, loc: &mut Location) -> Result<Vec<Statement>, String> {
-	let mut ast = Vec::<Statement>::new();
-
-	loop {
-		match lex(input, loc)? {
-			Token::RightCurlyBrace => break,
-			token @ _ => ast.push(Statement::Expression(parse_expression(token, input, loc)?))
-		}
-	}
-
-	Ok(ast)
-}
-
-fn parse_root(input: &mut Peekable<Chars>, loc: &mut Location) -> Result<Vec<Statement>, String> {
-	let mut ast = Vec::<Statement>::new();
-
-	loop {
-		match lex(input, loc)? {
-			Token::Environment => {
-				let name = expect_identifier(input, loc)?;
-				expect_lbrace(input, loc)?;
-
-				ast.push(Statement::Environment(Environment {
-					name,
-					contents: parse_environment(input, loc)?
-				}));
-			}
-
-			Token::Include => {
-				match lex(input, loc)? {
-					Token::String(..) => todo!(),
-					Token::Asm => {
-						ast.push(Statement::InlineAssembly(format!(
-							"include \"{}\"",
-							expect_string(input, loc)?
-						)));
-					}
-					token @ _ => return Err(format!("Expected `asm` after `include`, got {token}"))
-				}
-			}
-
-			Token::Identifier(identifier) => {
-				match lex(input, loc)? {
-					Token::Function => {
-						let name = expect_identifier(input, loc)?;
-						expect_lbrace(input, loc)?;
-
-						ast.push(Statement::Function(Function {
-							name,
-							environment: identifier,
-							contents: parse_function(input, loc)?
-						}));
-					}
-					_ => {
-						return Err(format!("Unexpected identifier \"{identifier}\""));
-					}
-				}
-			}
-
-			Token::InlineAssembly(assembly) => {
-				ast.push(Statement::InlineAssembly(assembly));
-			}
-
-			Token::Eof => break,
-			token @ _ => return Err(format!("Unexpected {token}"))
-		}
-	}
-
-	Ok(ast)
-}
-
-pub fn parse(input: &mut Peekable<Chars>, loc: &mut Location) -> Result<Vec<Statement>, String> {
-	parse_root(input, loc)
 }
